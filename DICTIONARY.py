b3010d4906549f34651bb1104f94747686fa30eb
Lokesh Joshi <69955726+lokeshjoshi01@users.noreply.github.com>
2020-08-20 13:30:26 +0530

    Add files via upload

---
+++ DICTIONARY.py
@@ -0,0 +1,11 @@
+print("enter the word")
+d = {"inmutable":"cannot change","mutable":"can change","python":"java","pubg":"free fire"}
+n1 = input()
+print(d.get(n1))
+n2 = input()
+print(d.get(n2))
+n3 = input()
+print(d.get(n3))
+n4 = input()
+print(d.get(n4))
+
